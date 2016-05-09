d1d182676b06612ff4cb28822823a0a6904cdecf
Manish Thani <manishthani@MacBook-Pro-de-Manish.local>
2016-05-09 12:32:21 +0200

    Adding Menu Scene, Colliders and some GameObjects

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public Button startText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+		
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+	}
+
+	void Update() {
+		
+	}
+	public void exitPress() {
+		Debug.Log("EXIT ");
+		Application.Quit ();
+	}
+
+	public void startGame() {
+		Debug.Log ("STARTT");
+		Application.LoadLevel ("PinchingSandbox");
+
+	}
+
+}
+
