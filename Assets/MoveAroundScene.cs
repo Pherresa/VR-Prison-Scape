7d2166cf3bfb550cdbcfcf389918a01317d10e52
Manish Thani <manishthani@MacBook-Pro-de-Manish.local>
2016-05-11 20:56:07 +0200

    Added Pipe Breaking with Hammer,Dummy Puzzle,Particle System, Remove Books Puzzle and Cleaned Project a bit

---
+++ Assets/MoveAroundScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAroundScene : MonoBehaviour {
+
+	public Transform camera;
+	public float speed;
+	bool spotTouched = false;
+
+	void Update() {
+		if (spotTouched){
+			float step = speed * Time.deltaTime;
+			print (this.transform.position);
+			camera.position = Vector3.MoveTowards(camera.position, transform.position, step);
+			if (camera.position == transform.position) {
+				spotTouched = false;
+
+			}
+		}
+	}
+	// Use this for initialization
+	void OnMouseDown() {
+		spotTouched = true;
+	}
+}
